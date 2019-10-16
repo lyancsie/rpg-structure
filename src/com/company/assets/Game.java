cfd703deace8691fa98aad4af5987f216b4ec402
lyancsie <info@fordit.biz>
2019-10-16 03:58:44 +0200

    started to implement Game class

--- src/com/company/assets/Game.java
+++ src/com/company/assets/Game.java
@@ -1,18 +1,41 @@
 package com.company.assets;
 
+import com.company.assets.enums.Difficulty;
+
 //TODO: use some library to implement JSON toString();
 public class Game {
-  //TODO: create model
-  int a = 25;
-  String name = "Abc";
 
-  public static Game newGame() {
-    return new Game();
+  //TODO: create model
+  //TODO: two-player model? --> how?
+
+  private static Game instance;
+  private Board board;
+  private final Difficulty difficulty;
+
+
+  public Board getBoard() {
+    return board;
+  }
+
+  public Difficulty getDifficulty() {
+    return difficulty;
+  }
+
+  private Game(Difficulty difficulty) {
+    this.difficulty = difficulty;
+  }
+
+  public static Game newGame(Difficulty difficulty) {
+    return (instance == null ? new Game(difficulty) : instance);
   }
 
   public static Game loadGame() {
-    return new Game();
+    return instance;
   }
+
+  int a = 25;
+
+  String name = "Abc";
 
   @Override
   public String toString() {
